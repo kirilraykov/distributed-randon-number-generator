55fecd7c38aafed7f10d64c08686b249ef235adf
Kiril Raykov <kirilraykov@gmail.com>
2023-10-31 15:00:44 +0200

    Add console input support

--- src/main/java/com/kiril/demo/DistributedNumberGeneratorDemo.java
+++ src/main/java/com/kiril/demo/DistributedNumberGeneratorDemo.java
@@ -1,4 +1,4 @@
-package com.kiril;
+package com.kiril.demo;
 
 import com.kiril.generator.DistributionRandomNumberGenerator;
 import com.kiril.generator.RandomNumberGenerator;
@@ -6,16 +6,31 @@ import com.kiril.generator.RandomNumberGenerator;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static com.kiril.utils.InputProcessor.*;
 
-public class UsageDemo {
+public class DistributedNumberGeneratorDemo {
 
     public static void main(String[] args) {
         // As per requirement, the random generator should work based on the provided allowed numbers and their probability lists
-        List<Integer> allowedNumbers = List.of(-1, 0, 1, 2, 3);
-        List<Double> probabilities = List.of(0.01, 0.3, 0.58, 0.1, 0.01);
+        Scanner scanner = new Scanner(System.in);
+
+        // Read a line for the list of allowed numbers
+        System.out.println("Enter a list of integers separated by spaces:");
+        String intLine = scanner.nextLine();
+        List<Integer> allowedNumbers = Arrays.stream(intLine.trim().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        // Read a line for the list of probabilities
+        System.out.println("Enter a list of doubles separated by spaces:");
+        String doubleLine = scanner.nextLine();
+        List<Double> probabilities = Arrays.stream(doubleLine.trim().split("\\s+"))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
         validateInputLists(allowedNumbers, probabilities);
 
         // Convert the valid lists to numbers distribution map
@@ -27,6 +42,7 @@ public class UsageDemo {
         // Run the number generator n times and save into generated number + # of occurrences map
         Map<Integer, Integer> numberOccurrencesMap = new HashMap<>();
         int iterations = 10000;
+        System.out.printf("Generating %d random numbers using the provided numbers distribution...\n", iterations);
         IntStream.range(0, iterations)
                 .map(i -> numberGenerator.nextNum())
                 .forEach(num -> numberOccurrencesMap.put(num, (numberOccurrencesMap.get(num) == null) ? 1 : numberOccurrencesMap.get(num) + 1));
@@ -39,5 +55,4 @@ public class UsageDemo {
             System.out.printf("Number of occurrences for %d: %d. Probability: ~%.5f\n", key, value, scaledProbability);
         });
     }
-
 }
